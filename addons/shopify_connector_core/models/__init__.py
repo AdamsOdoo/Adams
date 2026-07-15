26fdadb8aded14c75bac46109281f7cdd52586ae
AdamsOdoo <anashussein138@gmail.com>
2026-07-15 19:46:39 +0400

    feat(job-actions): register additive job actions

--- addons/shopify_connector_core/models/__init__.py
+++ addons/shopify_connector_core/models/__init__.py
@@ -5,6 +5,7 @@ from . import shopify_connector_store_settings
 from . import shopify_connector_location
 from . import shopify_connector_binding_mixin
 from . import shopify_connector_job
+from . import shopify_connector_job_actions
 from . import shopify_connector_job_enqueue
 from . import shopify_connector_call_lease
 from . import shopify_connector_job_dispatch
