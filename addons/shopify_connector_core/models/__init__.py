219f11af4258d1dcc7245ca87484ef770ec417c4
AdamsOdoo <anashussein138@gmail.com>
2026-07-19 13:58:47 +0400

    chore(layer2): register mutation attempt model

--- addons/shopify_connector_core/models/__init__.py
+++ addons/shopify_connector_core/models/__init__.py
@@ -6,6 +6,7 @@ from . import shopify_connector_pii_retention
 from . import shopify_connector_location
 from . import shopify_connector_binding_mixin
 from . import shopify_connector_job
+from . import shopify_connector_mutation_attempt
 from . import shopify_connector_job_actions
 from . import shopify_connector_job_enqueue
 from . import shopify_connector_call_lease
