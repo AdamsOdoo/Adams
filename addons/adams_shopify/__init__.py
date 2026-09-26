aa27cb1b5eb1cce6d6010501633b0ebb21a59145
AdamsOdoo <anashussein138@gmail.com>
2026-09-26 18:52:36 +0400

    Delete addons/adams_shopify directory

--- addons/adams_shopify/__init__.py
+++
@@ -1,6 +0,0 @@
-# Part of Shopify Connector Pro. See LICENSE file for full copyright and licensing details.
-from . import models
-from . import shopify_api
-from . import sync
-from . import controllers
-from . import wizards
--- addons/adams_shopify/controllers/__init__.py
+++
@@ -1,2 +0,0 @@
-# Part of Shopify Connector Pro. See LICENSE file for full copyright and licensing details.
-from . import webhook
